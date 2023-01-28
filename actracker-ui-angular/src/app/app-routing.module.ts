a8af5ac15257e59aadab44224af1377baf26579c
marcinciapa <aaryndo@gmail.com>
2023-01-28 22:01:12 +0100

    Add login form and create session in local storage (#6)

--- actracker-ui-angular/src/app/app-routing.module.ts
+++ actracker-ui-angular/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ActivityListComponent } from './activity-list/activity-list.component';
+import { LoginComponent } from './login/login.component';
 
 
 // TODO [mc] visit https://angular.io/tutorial/toh-pt5 to improve routing
 const routes: Routes = [
   { path: 'activities', component: ActivityListComponent },
+  { path: 'login', component: LoginComponent },
 ];
 
 @NgModule({
