42851f1e59b70661c662c4880ba78ef6ed158fa4
Marcin Ciapa <aaryndo@gmail.com>
2023-05-08 22:44:46 +0200

    Drill down on dashboard

--- actracker-ui-angular/src/app/dashboard-data/dashboard-data.component.ts
+++ actracker-ui-angular/src/app/dashboard-data/dashboard-data.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { DashboardDataService } from '../dashboard-data.service';
 import { TagService } from '../tag.service';
@@ -24,7 +25,8 @@ export class DashboardDataComponent implements OnInit {
 
   constructor(
     private dashboardDataService: DashboardDataService,
-    private tagService: TagService
+    private tagService: TagService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
@@ -75,6 +77,47 @@ export class DashboardDataComponent implements OnInit {
     dayBuckets.forEach(bucket => {
       bucket.label = `${bucket.rangeStart?.toLocaleString()} - ${bucket.rangeEnd?.toLocaleString()}`;
     });
+  }
+
+  drillDown(bucket: BucketData): void {
+    let tagBuckets: BucketData[] = this.getBucketsOfType('TAG', [bucket]);
+    let dailyBuckets: BucketData[] = this.getBucketsOfType('DAY', [bucket]);
+
+    let tags: Tag[] = tagBuckets
+      .filter(bucket => !!bucket.id)
+      .map(bucket => <Tag>{id: bucket.id});
+
+    let rangeStarts: Date[] = dailyBuckets
+      .filter(bucket => !!bucket.rangeStart)
+      .map(bucket => bucket.rangeStart!)
+
+    if(this.activityFilter.dateRangeStart) {
+      rangeStarts.unshift(this.activityFilter.dateRangeStart);
+    }
+
+    let latestRangeStart: Date | undefined = rangeStarts?.length > 0
+      ? rangeStarts.reduce((date1, date2) => date1! > date2! ? date1 : date2)
+      : undefined;
+
+    let rangeEnds: Date[] = dailyBuckets
+      .filter(bucket => !!bucket.rangeEnd)
+      .map(bucket => bucket.rangeEnd!)
+
+    if(this.activityFilter.dateRangeEnd) {
+      rangeEnds.unshift(this.activityFilter.dateRangeEnd);
+    }
+
+    let earliestRangeEnd: Date | undefined = rangeEnds?.length > 0
+      ? rangeEnds.reduce((date1, date2) => date1! < date2! ? date1 : date2)
+      : undefined;
+
+    let optionalParams = {
+      tags: tags.length > 0 ? tags.map(tag => tag.id).join(',') : '',
+      rangeStart: latestRangeStart?.getTime() ?? '',
+      rangeEnd: earliestRangeEnd?.getTime() ?? ''
+    }
+
+    this.router.navigate(['/activities', optionalParams]);
   }
 
 }
