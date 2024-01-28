91a16a7a4aff573a43aa823f64163a60ea85dd2d
Marcin Ciapa <aaryndo@gmail.com>
2024-01-28 13:08:38 +0100

    Update actracker-api base url (append /api) (#66)

--- actracker-ui-angular/src/environments/environment.dev.ts
+++ actracker-ui-angular/src/environments/environment.dev.ts
@@ -1,5 +1,5 @@
 export const environment = {
   environment: 'dev',
   production: true,
-  backendBaseUrl: 'https://dev.cloud.equino.ovh/actracker-api'
+  backendBaseUrl: 'https://dev.cloud.equino.ovh/actracker-api/api'
 };
