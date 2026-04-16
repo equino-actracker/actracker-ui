9f5ebafa37063506b509fd157528225199d9fc75
Marcin Ciapa <aaryndo@gmail.com>
2026-04-16 22:43:56 +0200

    Disable default sorting for activities

--- actracker-ui-angular/src/app/activity.service.ts
+++ actracker-ui-angular/src/app/activity.service.ts
@@ -24,7 +24,7 @@ export class ActivityService {
 
   searchActivities(term?: String, pageId?: String, pageSize?: number, excludedActivities?: Activity[], dateRangeStart?: Date, dateRangeEnd?: Date, tags?: Tag[]): Observable<ActivitiesResult> {
     let url: string = `${environment.backendBaseUrl}/activity/matching`;
-    let requestParams = ['orderBy=START_TIME.DESC'];
+    let requestParams = [];
     if(!!term) {
       requestParams.unshift(`term=${term}`)
     }
