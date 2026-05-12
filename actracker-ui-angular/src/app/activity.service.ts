e585cef61271993f3b944a59ac8d963cb7b8e45e
Marcin Ciapa <aaryndo@gmail.com>
2026-05-12 19:41:22 +0200

    Sort activities by title

--- actracker-ui-angular/src/app/activity.service.ts
+++ actracker-ui-angular/src/app/activity.service.ts
@@ -24,7 +24,7 @@ export class ActivityService {
 
   searchActivities(term?: String, pageId?: String, pageSize?: number, excludedActivities?: Activity[], dateRangeStart?: Date, dateRangeEnd?: Date, tags?: Tag[]): Observable<ActivitiesResult> {
     let url: string = `${environment.backendBaseUrl}/activity/matching`;
-    let requestParams = [];
+    let requestParams = ['orderBy=TITLE.ASC'];
     if(!!term) {
       requestParams.unshift(`term=${term}`)
     }
